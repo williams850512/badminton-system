01ca2753cda2c7f22a585a1a3df41d4398ab3713
RUI <a83412367@gmail.com>
2026-04-11 15:20:33 +0800

    完成Venues改寫、application-local.yml新增設定。

---
+++ src/main/java/com/badminton/venue/VenueController.java
@@ -0,0 +1,10 @@
+package com.badminton.venue;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/venues")
+public class VenueController {
+
+}
---
+++ src/main/java/com/badminton/venue/VenueRepository.java
@@ -0,0 +1,9 @@
+package com.badminton.venue;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VenueRepository extends JpaRepository<Venue, Integer> {
+
+}
