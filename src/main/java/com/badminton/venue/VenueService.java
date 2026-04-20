7a95721b086f90a590219341fb3fb9d2a46fbad3
RUI <a83412367@gmail.com>
2026-04-20 22:03:09 +0800

    完成登出鍵正確連結、首頁左上角圖案變更。

--- src/main/java/com/badminton/venue/VenueService.java
+++ src/main/java/com/badminton/venue/VenueService.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
+
 @Service
+@Transactional
 public class VenueService {
 
 	@Autowired
